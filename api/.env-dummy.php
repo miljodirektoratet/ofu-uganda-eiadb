5abbf6b1c0a3eee26cf4e89e7cbbea672be7594f
Jostein Skaar <jostein.skaar@gmail.com>
2014-05-23 15:43:44 +0200

    making sure dummy env file is included

---
+++ api/.env-dummy.php
@@ -0,0 +1,16 @@
+<?php
+
+// Rename to one of the following:
+// Development: .env.local.php
+// Test server: .env.test.php
+// Production server: .env.php
+
+return array(
+
+		'app_url' => 'https://uganda',
+		'app_debug' => true,
+    'mysql_database' => 'databasename',
+    'mysql_username' => 'username',
+    'mysql_password' => 'password',
+
+);
