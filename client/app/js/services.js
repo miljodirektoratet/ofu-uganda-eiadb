9423237be2752cba9f98595631dddda70232e3c9
Jostein Skaar <jostein.skaar@gmail.com>
2014-07-04 16:35:33 +0200

    Lazy commit all.

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -2,7 +2,7 @@
 
 /* Services */
 
-var version = {"version": "0.0.15"};
+var version = {"version": "0.0.16"};
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
