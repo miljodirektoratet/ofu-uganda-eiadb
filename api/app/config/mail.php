f593d2d26d8d5732439a10068abe89e916dffd58
unknown <JOSSKA@PC-JOSSKA.pax.local>
2014-08-15 14:27:33 +0300

    mail

--- api/app/config/mail.php
+++ api/app/config/mail.php
@@ -1,24 +1,13 @@
 <?php
 
+
 return array(
 	'driver' => 'smtp',
 	'host' => 'smtp.mailgun.org',
 	'port' => 587,	
-	'from' => array('address' => "noreply@sandboxb057a0588a384581ab7b50a5f5a74a78.mailgun.org", 'Mailgun Sandbox' => null),
+	'from' => array('address' => "noreply@sandboxb057a0588a384581ab7b50a5f5a74a78.mailgun.org", 'name' => null),
 	'encryption' => 'tls',
 	'username' => 'postmaster@sandboxb057a0588a384581ab7b50a5f5a74a78.mailgun.org',
 	'password' => '2x70e0bh4gl6',
 	'pretend' => false,
 );
-/*
-return array(
-	'driver' => 'smtp',
-	'host' => 'smtp.mailgun.org',
-	'port' => 587,
-	'from' => array('address' => "noreply@sandboxb057a0588a384581ab7b50a5f5a74a78.mailgun.org", 'Mailgun Sandbox' => null),
-	'encryption' => 'tls',
-	'username' => 'postmaster@sandboxb057a0588a384581ab7b50a5f5a74a78.mailgun.org',
-	'password' => '2x70e0bh4gl6',
-	'sendmail' => '/usr/sbin/sendmail -bs',
-	'pretend' => false
-);*/
