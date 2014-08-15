24396f1e563450181d9280faf3fdd5d28f42987b
unknown <JOSSKA@PC-JOSSKA.pax.local>
2014-08-15 12:23:45 +0300

    Lazy commit all.

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -2,7 +2,7 @@
 
 /* Services */
 
-var version = {"version": "0.0.55"};
+var version = {"version": "0.0.56"};
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
