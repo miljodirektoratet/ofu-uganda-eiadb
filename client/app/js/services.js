6c3245ab87e93b01a27bf6f860bfd3871dd884de
Jostein Skaar <jostein.skaar@gmail.com>
2015-01-20 12:43:38 +0100

    New version.

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -2,7 +2,7 @@
 
 /* Services */
 
-var version = {"version": "0.0.77"};
+var version = {"version": "0.0.78"};
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
