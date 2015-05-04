d51447e4332b1dc6ce604ea80eefbdf307247c3e
Jostein Skaar <jostein.skaar@gmail.com>
2015-05-04 14:18:33 +0200

    Release v0.1.11

---
+++ api/app/Support/helpers.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Input;
+
+if ( ! function_exists('getSearchCriterias'))
+{
+
+
+  /**
+   * Assign high numeric IDs to a config item to force appending.
+   *
+   * @param  array  $array
+   * @return array
+   */
+  function getSearchCriterias(array $words)
+  {
+    $criterias = [];
+    foreach ($words as $word) 
+    {
+      if (Input::has($word))
+      {
+        $criterias[$word] = Input::get($word);
+      }
+    }
+    return $criterias;
+  }
+}
