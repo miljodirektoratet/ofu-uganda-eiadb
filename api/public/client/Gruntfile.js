f27774721c997677ef5c1f715dda5ce562723668
Jostein Skaar <jostein.skaar@gmail.com>
2015-09-16 15:57:23 +0200

    Release v0.1.22

--- api/public/client/Gruntfile.js
+++ api/public/client/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt)
         pkg: grunt.file.readJSON('package.json'),
 
         paths: {
-            buildPath: '../build/'
+            buildPath: '../app/'
         },
 
         generatedBanner: '// Generated with grunt. Do not edit!\n\n'
--- api/public/client/js/services.js
+++ api/public/client/js/services.js
@@ -2,7 +2,7 @@
 
 /* Services */
 
-var version = {"version": "0.1.21"};
+var version = {"version": "0.1.22"};
 
 var services = angular.module('seroApp.services');
 
