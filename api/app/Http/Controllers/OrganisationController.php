7499f75016e8a997c709dfdf588c028cf66e4f6f
Jostein Skaar <jostein.skaar@gmail.com>
2015-09-21 21:05:26 +0200

    Save created_by when creating new.

--- api/app/Http/Controllers/OrganisationController.php
+++ api/app/Http/Controllers/OrganisationController.php
@@ -6,123 +6,123 @@ use Input;
 use \DateTime;
 use \App\Organisation;
 
-class OrganisationController extends Controller {
+class OrganisationController extends Controller
+{
 
-  // GET /resource
-  public function index()
-  { 
-    $organisations = Organisation::            
-      get(array('id', 'name', 'visiting_address', 'city', 'contact_person'));
-  
-    return Response::json($organisations->toArray(), 200); 
-  }
-
-  // GET /resource/:id
-  public function show($id)
-  {   
-    $organisation = Organisation::find($id);   
-    return Response::json($organisation, 200);
-  }
-
-  // POST /resource
-  public function store()
-  {       
-    if (!$this::canSave())
+    // GET /resource
+    public function index()
     {
-      return $this::notAuthorized();
+        $organisations = Organisation::
+        get(array('id', 'name', 'visiting_address', 'city', 'contact_person'));
+
+        return Response::json($organisations->toArray(), 200);
     }
 
-    $inputData = Input::all();
-    $organisation = new Organisation();     
-    $this->updateValuesInResource($organisation, $inputData);   
-    $organisation->save();    
-
-    return $this->show($organisation->id);
-  } 
-
-  // PUT/PATCH /resource/:id
-  public function update($id)
-  {   
-    if (!$this::canSave())
+    // GET /resource/:id
+    public function show($id)
     {
-      return $this::notAuthorized();
+        $organisation = Organisation::find($id);
+        return Response::json($organisation, 200);
     }
 
-    $organisation = Organisation::find($id);
-    if (!$organisation)
+    // POST /resource
+    public function store()
     {
-      return Response::json(array('error' => true, 'message' => 'not found'), 404);
-    }
-
-    $inputData = Input::all();    
-    $this->updateValuesInResource($organisation, $inputData);
-    $organisation->save();    
-    return $this->show($organisation->id);
-  }
-
-  // DELETE /resource/:id
-  public function destroy($id)
-  {
-    if (!$this::canSave())
-    {
-      return $this::notAuthorized();
-    }
-
-    $organisation = Organisation::find($id);  
-    $organisation->delete();
-    return Response::json(array('is_deleted' => true), 200);
-  }
-
-  private function updateValuesInResource($resource, $data)
-  {   
-    $dates = $resource->getDates();
-    $changed = false;
-    foreach ($data as $key => $value)
-    {     
-      if (in_array($key, $resource["fillable"], true))
-      {       
-        if ($value === "")
+        if (!$this::canSave())
         {
-          $value = null;
+            return $this::notAuthorized();
         }
-        if ($value && in_array($key, $dates))
-        {
-          $timestamp = strtotime($value);
-          if ($timestamp === false)
-          {
-            $value = null;
-          }
-          else
-          {
-            $value = new DateTime();
-            $value->setTimestamp($timestamp);
-          }
-        }         
 
-        if ($resource[$key] != $value)
-        {         
-          // TODO: Validate.          
-          $resource[$key] = $value;
-          $changed = true;
-        }               
-      }       
+        $inputData = Input::all();
+        $organisation = new Organisation();
+        $this->updateValuesInResource($organisation, $inputData);
+        $organisation->created_by = Auth::user()->name;
+        $organisation->save();
+
+        return $this->show($organisation->id);
     }
-    if ($changed)
+
+    // PUT/PATCH /resource/:id
+    public function update($id)
     {
-      $resource["updated_by"] = Auth::user()->name;
-      //$project->created_by = Auth::user()->name;
+        if (!$this::canSave())
+        {
+            return $this::notAuthorized();
+        }
+
+        $organisation = Organisation::find($id);
+        if (!$organisation)
+        {
+            return Response::json(array('error' => true, 'message' => 'not found'), 404);
+        }
+
+        $inputData = Input::all();
+        $this->updateValuesInResource($organisation, $inputData);
+        $organisation->save();
+        return $this->show($organisation->id);
     }
-  }
 
-  private function canSave()
-  {
-    // TODO: grade needs Role 7.
-    return Auth::user()->hasRole("Role 1");
-  }
+    // DELETE /resource/:id
+    public function destroy($id)
+    {
+        if (!$this::canSave())
+        {
+            return $this::notAuthorized();
+        }
 
-  private function notAuthorized()
-  {   
-    return Response::json("Not authorized to perform this.", 403); // 403 Forbidden
-  }
+        $organisation = Organisation::find($id);
+        $organisation->delete();
+        return Response::json(array('is_deleted' => true), 200);
+    }
+
+    private function updateValuesInResource($resource, $data)
+    {
+        $dates = $resource->getDates();
+        $changed = false;
+        foreach ($data as $key => $value)
+        {
+            if (in_array($key, $resource["fillable"], true))
+            {
+                if ($value === "")
+                {
+                    $value = null;
+                }
+                if ($value && in_array($key, $dates))
+                {
+                    $timestamp = strtotime($value);
+                    if ($timestamp === false)
+                    {
+                        $value = null;
+                    } else
+                    {
+                        $value = new DateTime();
+                        $value->setTimestamp($timestamp);
+                    }
+                }
+
+                if ($resource[$key] != $value)
+                {
+                    // TODO: Validate.
+                    $resource[$key] = $value;
+                    $changed = true;
+                }
+            }
+        }
+        if ($changed)
+        {
+            $resource["updated_by"] = Auth::user()->name;
+        }
+    }
+
+    private function canSave()
+    {
+        // TODO: grade needs Role 7.
+        return Auth::user()->hasRole("Role 1");
+    }
+
+    private function notAuthorized()
+    {
+        return Response::json("Not authorized to perform this.", 403); // 403 Forbidden
+    }
 
 }
