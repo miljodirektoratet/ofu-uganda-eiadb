651d788f6c752263725c6ad38b8f3e96928d9ee2
Jostein Skaar <jostein.skaar@gmail.com>
2015-09-24 09:52:46 +0200

    Directive setFocus

--- api/public/client/js/directives.js
+++ api/public/client/js/directives.js
@@ -3,84 +3,85 @@
 /* Directives */
 
 var directives = angular.module('seroApp.directives').
-  directive('appVersion', ['version', function (version)
-  {
-    return function (scope, elm, attrs)
+    directive('appVersion', ['version', function (version)
     {
-      elm.text(version.replace(/\.0$/, '').replace(/\.00$/, '.0'));
-    };
-  }]);
+        return function (scope, elm, attrs)
+        {
+            elm.text(version.replace(/\.0$/, '').replace(/\.00$/, '.0'));
+        };
+    }]);
 
 
 directives.directive("addPractitionerButton", ['Practitioner', function (Practitioner)
 {
-  return {
-    restrict: "A",
-    link: function (scope, element, attrs)
-    {
-      element.bind("click", function ()
-      {
-        var newTest = new Practitioner({"person": "Gunnar Nymann", "city": "Oslo"});
-        newTest.$save();
-      });
-    }
-  }
-}]);
-
-directives.directive('animateNewElement', ['$animate', function(animate)
-{
-  return function(scope, element, attrs)
-  {
-    scope.$watch(attrs.animateNewElement, function(nv,ov)
-    {
-      if (nv === true)
-      {
-        console.log(element);
-        var className = 'new-element';
-        animate.addClass(element, className, function ()
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs)
         {
-          animate.removeClass(element, className);
-        });
-      }
-    })
-  }
-}]);
-
-directives.directive('hoverOnParent', [function()
-{
-  return {
-    restrict: "A",
-    link: function (scope, element, attrs)
-    {
-      var selector = attrs.hoverOnParent;
-      element.parent(selector).bind('mouseenter', function ()
-      {
-        element.addClass('btn-link-hover');
-      });
-      element.parent(selector).bind('mouseleave', function ()
-      {
-        element.removeClass('btn-link-hover');
-      });
+            element.bind("click", function ()
+            {
+                var newTest = new Practitioner({"person": "Gunnar Nymann", "city": "Oslo"});
+                newTest.$save();
+            });
+        }
     }
-  }
+}]);
+
+directives.directive('animateNewElement', ['$animate', function (animate)
+{
+    return function (scope, element, attrs)
+    {
+        scope.$watch(attrs.animateNewElement, function (nv, ov)
+        {
+            if (nv === true)
+            {
+                console.log(element);
+                var className = 'new-element';
+                animate.addClass(element, className, function ()
+                {
+                    animate.removeClass(element, className);
+                });
+            }
+        })
+    }
+}]);
+
+directives.directive('hoverOnParent', [function ()
+{
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs)
+        {
+            var selector = attrs.hoverOnParent;
+            element.parent(selector).bind('mouseenter', function ()
+            {
+                element.addClass('btn-link-hover');
+            });
+            element.parent(selector).bind('mouseleave', function ()
+            {
+                element.removeClass('btn-link-hover');
+            });
+        }
+    }
 }]);
 
 
-directives.directive('coordinatePasting', function() {
-  return {
-    require: 'ngModel',
+directives.directive('coordinatePasting', function ()
+{
+    return {
+        require: 'ngModel',
 
-    // What to do to make this work?
+        // What to do to make this work?
 
-    link: function(scope, elm, attrs, ctrl)
-    {
+        link: function (scope, elm, attrs, ctrl)
+        {
 
-      //console.log("viewValue", viewValue);
-      //0.356104, 32.581819
-      //viewValue='0.356104';
-      console.log("here");
-      ctrl.$setViewValue('0.12104');
-      ctrl.$render();
+            //console.log("viewValue", viewValue);
+            //0.356104, 32.581819
+            //viewValue='0.356104';
+            //console.log("here");
+            ctrl.$setViewValue('0.12104');
+            ctrl.$render();
 //
 //      ctrl.$parsers.unshift(function(viewValue)
 //      {
@@ -93,17 +94,25 @@ directives.directive('coordinatePasting', function() {
 //        console.log("modelValue", modelValue);
 //        return modelValue;
 //      });
-    }
-  };
+        }
+    };
 });
 
-directives.directive('focus', function ()
+
+
+directives.directive('setFocus', ['$timeout', function ($timeout)
 {
-  return function (scope, element, attrs)
-  {
-    attrs.$observe('focus', function (newValue)
-    {
-      newValue === 'true' && element[0].focus();
-    });
-  }
-});
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs)
+        {
+            $timeout(function ()
+            {
+                $timeout(function()
+                {
+                    element[0].focus();
+                })
+            })
+        }
+    };
+}]);
