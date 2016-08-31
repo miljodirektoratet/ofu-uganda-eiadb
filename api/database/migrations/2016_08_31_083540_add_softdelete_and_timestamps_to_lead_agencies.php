eae91d11c236099f1d07917b72210ee09ca39f2e
Jostein Skaar <jostein.skaar@gmail.com>
2016-08-31 10:52:33 +0200

    Release v3.13.0

--- api/database/migrations/2016_08_31_083540_add_softdelete_and_timestamps_to_lead_agencies.php
+++ api/database/migrations/2016_08_31_083540_add_softdelete_and_timestamps_to_lead_agencies.php
@@ -18,7 +18,7 @@ class AddSoftdeleteAndTimestampsToLeadAgencies extends Migration
             $table->string('created_by', 255)->nullable()->default('SYSTEM');
             $table->string('updated_by', 255)->nullable()->default('SYSTEM');
 //            $table->timestamps();
-            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at');//->default(\DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
         });
     }
