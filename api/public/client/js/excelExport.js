f09dcd7b21711e227731177398b4d3a039bd5474
eddymens <kwasiamantin@gmail.com>
2019-04-16 17:24:10 +0000

    remove debug script

--- api/public/client/js/excelExport.js
+++ api/public/client/js/excelExport.js
@@ -5,8 +5,7 @@ var excelExport = function(data, dataMeta) {
   data = exportHelpers.renameFields(data, dataMeta["fieldmap"]);
   data = exportHelpers.sortObj(data, Object.values(dataMeta["fieldmap"]));
   data = exportHelpers.replaceNullValues(data);
-  console.log(data);
-  return;
+
   var excel = $("#dvjson").excelexportjs({
     containerid: "dvjson",
     datatype: "json",
