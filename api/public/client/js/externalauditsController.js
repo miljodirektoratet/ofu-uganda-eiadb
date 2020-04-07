c771a67758df9820b84db47cd46e56ebd30eb240
eddymens <kwasiamantin@gmail.com>
2020-04-07 07:07:02 +0200

    #223 fix index err

--- api/public/client/js/externalauditsController.js
+++ api/public/client/js/externalauditsController.js
@@ -131,9 +131,12 @@ controllers.controller('ExternalAuditsController', ['$scope', 'ProjectFactory', 
     };
 
     scope.filterByDateSubmission = function(data) {
+        if(!data.length && !data[0].documents){return;}
         data.sort(function(a,b){
-            var date1 =  a.documents[0].date_submitted
-            var date2 = b.documents[0].date_submitted
+            var date1 =  (a.documents)?a.documents[0].date_submitted:"";
+
+            var date2 = (b.documents)?b.documents[0].date_submitted:"";
+
             return date1 < date2 ? -1 : 1;
           })
         return data.reverse();
