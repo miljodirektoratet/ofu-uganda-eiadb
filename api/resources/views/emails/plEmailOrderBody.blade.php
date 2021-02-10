5b481c1c3bd0f4e3e105097fef5d2a22de5b3903
eddymens <kwasiamantin@gmail.com>
2021-02-10 10:15:03 +0100

    modify email template

--- api/resources/views/emails/plEmailOrderBody.blade.php
+++ api/resources/views/emails/plEmailOrderBody.blade.php
@@ -1,7 +1,7 @@
-We acknowledge receipt of your submission for project <b>{{$projectTitle}}</b>. For follow-up, your
-reference number is <b>{{$permitLicensesId}}</b> and the application number
-<b>{{$permitLicensesApplicationNumber}}</b>.<br><br>
-10
+We acknowledge receipt of your submission for project {{$projectTitle}}. For follow-up, your
+reference number is {{$permitLicensesId}} and the application number
+{{$permitLicensesApplicationNumber}}.<br><br>
+
 If you have any questions, please contact us at submissions@nema.go.ug using the reference
 number in the subject. <br><br>
 This is an automatically generated email – do not reply to it.
