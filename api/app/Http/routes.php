70b6c4879a0efadb6cec779d91c009bfa0b88e47
eddymens <kwasiamantin@gmail.com>
2021-04-22 05:00:10 +0200

    update files

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -49,10 +49,20 @@ Route::group(['middleware' => 'auth'], function ()
 });
 
 
-Route::controllers([
-    'auth' => 'Auth\AuthController',
-    'password' => 'Auth\PasswordController',
-]);
+
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
 Route::group(['prefix' => 'user', 'middleware' => 'auth'], function ()
